bf651791309948edec1fbdbae435af0367496f65
jacek <simplycodestudio@gmail.com>
2019-11-17 14:40:13 +0100

    1st rev

---
+++ src/com/luv2code/jsf/validatedemo/StudentTwo.java
@@ -0,0 +1,49 @@
+package com.luv2code.jsf.validatedemo;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentTwo {
+
+    public String firstName;
+    public String lastName;
+    public String postalCode;
+    private int freePasses;
+
+    public StudentTwo() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+}
+
